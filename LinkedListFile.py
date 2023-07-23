2726a622cca7fa78a89816d90b3b144267d2b0da
Hogchops <31632064+Hogchops@users.noreply.github.com>
2023-07-22 22:52:56 -0400

    Create LinkedListFile.py

---
+++ LinkedListFile.py
@@ -0,0 +1,48 @@
+class Node:
+    
+    def __init__(self, data):
+        """
+        Create a node object that contains two variables. 
+        The data, and the next node in the linked list.
+
+        Parameters
+        ----------
+        data : int
+            The value of the ID in the database.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.data = data
+        self.next = None
+        
+
+class LinkedList:
+    
+    def __init__(self):
+        """
+        Initialization of the Linked List, defaulting to a null head.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.head = None
+        
+    def size():
+        # code goes here
+    
+    def index_of(value):
+        # code goes here
+    
+    def add_first(value):
+        # code goes here
+    
+    def remove(loc):
+        # code goes here
+    
+    def to_string():
+        # code goes here
